e34081a9ea3d9a5ad91f7c3a84d233af2c8997f9
Max <maksim.zelenovskiy@gmail.com>
2021-06-30 09:17:24 +0300

    30.06

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 $('.btn_nav_menu').click(function(){
     $('.nav_menu_mobile').toggleClass('active')
+    $('body').addClass("fixed-position");
 })
 $('.btn_nav_close').click(function(){
     $('.nav_menu_mobile').removeClass('active')
+    $('body').removeClass("fixed-position");
 })
+
+
+function resizeFooterLogo () {
+    if(window.innerWidth > 1440) {
+        const footerLogo = document.querySelector('.footer_logo')
+        let width = (window.innerWidth - 1440) / 2 + 427
+        footerLogo.style.width = width + 'px'
+    }
+
+}
+window.onresize = function(event) {
+    resizeFooterLogo ()
+};
+resizeFooterLogo ()
